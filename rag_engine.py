fb94d03c2f7734ab69a6e3ad484c71aa2e65ba09
MarceliBil <marbil003@gmail.com>
2025-10-18 20:16:15 +0200

    Dropbox access method change

--- rag_engine.py
+++ rag_engine.py
@@ -1,8 +1,6 @@
 import os
 import io
 import hashlib
-import requests
-import time
 from datetime import datetime
 import pdfplumber
 import docx2txt
@@ -15,43 +13,12 @@ load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-DROPBOX_APP_KEY = os.getenv("DROPBOX_APP_KEY")
-DROPBOX_APP_SECRET = os.getenv("DROPBOX_APP_SECRET")
-DROPBOX_REFRESH_TOKEN = os.getenv("DROPBOX_REFRESH_TOKEN")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-_cached_token = None
-_token_expires_at = 0
-
-
-def get_dropbox_token():
-    global _cached_token, _token_expires_at
-
-    if _cached_token and time.time() < _token_expires_at:
-        return _cached_token
-    
-    resp = requests.post(
-        "https://api.dropboxapi.com/oauth2/token",
-        data={
-            "grant_type": "refresh_token",
-            "refresh_token": DROPBOX_REFRESH_TOKEN,
-            "client_id": DROPBOX_APP_KEY,
-            "client_secret": DROPBOX_APP_SECRET,
-        },
-    )
-
-    data = resp.json()
-    _cached_token = data["access_token"]
-    _token_expires_at = time.time() + data.get("expires_in", 14400) - 60
-    return _cached_token
-
-
 def get_dropbox_client():
-    token = get_dropbox_token()
-    return dropbox.Dropbox(token)
-
+    return dropbox.Dropbox(os.getenv("DROPBOX_ACCESS_TOKEN"));
 
 def extract_text(file_bytes, filename):
     if filename.endswith(".pdf"):
